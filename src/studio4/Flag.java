6c1d91fa34e4a9bb18c9a6d7333b4acd967bd55b
andria-l <andrialuo1208@gmail.com>
2023-10-05 12:27:27 -0500

    Italy!

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -5,7 +5,16 @@ import java.awt.Color;
 import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
+	public static void italy() {
+		StdDraw.clear();
+		StdDraw.setPenColor(0,180,0);
+		StdDraw.filledRectangle(3, 4, 4, 4);
+		StdDraw.setPenColor(200,0,0);
+		StdDraw.filledRectangle(18, 4, 4, 4);
+	}
 	public static void main(String[] args) {
-		
+		StdDraw.setXscale(0,22);
+		StdDraw.setYscale(0,8);
+		italy();
 	}
 }
